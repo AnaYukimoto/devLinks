362ac0e53ef56dc32410440ec9952475d2fa6a76
Ana Yukimoto <anaelizayuki@gmail.com>
2024-01-30 18:14:27 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    /*if(body.classList.contains('light')){
+        html.classList.remove('light')
+    }
+    else{
+        html.classList.add('light')
+    }*/
+
+
+    //subtituir a img 
+    const img = document.querySelector("#profile img")
+    
+    if(html.classList.contains("light")){
+        img.setAttribute("src", "./assets/avatar-light.png")
+    }
+    else{
+        img.setAttribute("src", "./assets/avatar.png")
+    }
+}
